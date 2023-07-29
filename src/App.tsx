512ef486776ce53e0129ee392f4d6b9d7d8237d9
Vadim Fattakhov <vadfthv@gmail.com>
2023-07-29 15:05:36 -0500

    Basic JSX setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,98 @@
+import About from './components/About';
+import BookNow from './components/BookNow';
+import Home from './components/Home';
+import Services from './components/Services';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import {
+  HiOutlineLocationMarker,
+  HiOutlinePhone,
+  HiOutlineMail,
+} from 'react-icons/hi';
+import SubmitForm from './components/SubmitForm';
+
 function App() {
-  return <div className="my-div">YnV Beauty</div>;
+  return (
+    <div className="container">
+      <section className="home">
+        <header>
+          <div>
+            <img src={'../public/logo.png'} alt="" />
+          </div>
+          <RxHamburgerMenu />
+        </header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Book Now</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <section className="about">
+        <div>
+          <img src={'../public/logo.png'} alt="" />
+        </div>
+        <h1>An Incredibl SPa Experience in Sugar Land, Tx</h1>
+        <article>
+          <p>
+            Our licensed professionals build specific treatment program for
+            every guest.
+          </p>
+          <p>
+            We use only newest equipment and high quality medical products. Feel
+            yourself special with our individual approach!
+          </p>
+          <p>YnV Beauty is a Luxury Oasis for Your Skin!!!</p>
+        </article>
+      </section>
+      <section className="services">
+        <div className="service">
+          <h2>Classic Facial</h2>
+          <article>
+            For a deeper clean, this purifying treatment includes a double
+            exfoliating peel and a gentle botanical deep-cleansing mask to
+            rejuvenate the skin, improve cell turnover, and minimize pores. 60
+            minutes
+          </article>
+          <img src="" alt="" />
+        </div>
+      </section>
+      <footer>
+        <h1>Get in Touch With Us And We'll Help Your Skin!</h1>
+        <h3>
+          Interested in our spa services? We want your spa experience to be a
+          memorable one.
+        </h3>
+        <h2>Image Salon Studios</h2>
+        <div className="contacts">
+          <div className="contact">
+            <HiOutlineLocationMarker />
+            <address>
+              15315 Southwest Freeway, Suite QZ4 Sugar Land, TX, 77478
+            </address>
+          </div>
+          <div className="contact">
+            <HiOutlinePhone />
+            <a href="tel:+19793270505">+1 979 327 05 05</a>
+          </div>
+          <div className="contact">
+            <HiOutlineMail />
+            <a href="mailto: info@ynv-beauty.com">info@ynv-beauty.com</a>
+          </div>
+        </div>
+        <SubmitForm />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SubmitForm.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+function SubmitForm() {
+  const [name, setName] = useState('');
+  const [lastName, setlastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [tel, setTel] = useState('');
+  const [reason, setReason] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <form action="submit">
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="First Name*"
+        required
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setlastName(e.target.value)}
+        placeholder="Last Name"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email*"
+        required
+      />
+      <input
+        type="tel"
+        value={tel}
+        onChange={(e) => setTel(e.target.value)}
+        placeholder="Phone Number"
+      />
+      <input
+        type="text"
+        value={reason}
+        onChange={(e) => setReason(e.target.value)}
+        placeholder="Reason for Visit*"
+        required
+      />
+      <textarea
+        name="message"
+        id="message"
+        cols="30"
+        rows="10"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Your Message"
+      ></textarea>
+      <button>Submit Message</button>
+    </form>
+  );
+}
+
+export default SubmitForm;

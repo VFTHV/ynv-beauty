1c8ce18abe1ba4521bbb6d852bc0d2da4d749a9b
Vadim Fattakhov <vadfthv@gmail.com>
2023-07-30 01:25:17 -0500

    Animation of Menu

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,65 @@
+import { motion } from 'framer-motion';
+import { useState, useRef, useEffect, ChangeEvent } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNavMounted, setIsNavMounted] = useState(false);
+
+  const navRef = useRef<HTMLElement | null>(null);
+  const burgerMenuRef = useRef(null);
+
+  useEffect(() => {
+    setIsNavMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const closeMenuOnClickOutside = (e: MouseEvent) => {
+      if (
+        isMenuOpen &&
+        navRef.current &&
+        !navRef.current.contains(e.target as Node)
+      ) {
+        console.log(e.target);
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', closeMenuOnClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', closeMenuOnClickOutside);
+    };
+  }, [isMenuOpen]);
+
+  let navWidth = 224;
+
+  if (navRef.current) {
+    navWidth = navRef.current.getBoundingClientRect().width;
+  }
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
   return (
     <section id="home" className="home container">
       <header className="header">
         <div className="logo">
           <img src={'/logo.png'} alt="" />
         </div>
-        <RxHamburgerMenu color="white" size={30} className="menu" />
+        <RxHamburgerMenu
+          color="white"
+          size={30}
+          className="menu"
+          onClick={handleMenuToggle}
+        />
       </header>
-      <nav className="nav-head">
+      <motion.nav
+        className="nav-head"
+        animate={{
+          x: isMenuOpen ? navWidth : 0,
+        }}
+        ref={navRef}
+      >
         <ul className="nav-items">
           <li>
             <a href="#home">HOME</a>
@@ -26,7 +76,7 @@ function Home() {
             </a>
           </li>
         </ul>
-      </nav>
+      </motion.nav>
       <div className="home-content">
         <h1 className="h1">YnV Beauty</h1>
         <div className="cta-container">

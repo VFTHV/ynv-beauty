121de4eb0f30b57afa1fcd6de6dc95c744eb8db5
Vadim Fattakhov <vadfthv@gmail.com>
2023-07-30 11:26:23 -0500

    Setup for smooth scrolling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import { useState, useRef, useEffect, ChangeEvent } from 'react';
+import { motion, useScroll } from 'framer-motion';
+import { useState, useRef, useEffect } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 
 function Home() {

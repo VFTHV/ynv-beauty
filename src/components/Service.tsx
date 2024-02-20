75874fbd264664e503520acb2afac4d7759b9f72
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-20 10:45:30 -0600

    new services

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -8,6 +8,7 @@ interface ServiceProps {
 // new services
 // new services
 // new services
+// new services
 function Service({ service }: ServiceProps) {
   const renderParagraph = () => {
     if (Array.isArray(service.text)) {

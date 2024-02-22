60f222b62093d252717b34efdf4f8b74e5009552
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-22 05:41:44 -0600

    in view animate effect

--- src/components/InViewAnimate.tsx
+++ src/components/InViewAnimate.tsx
@@ -11,7 +11,6 @@ interface InViewAnimateProps {
 // in view animate effect
 // in view animate effect
 // in view animate effect
-// in view animate effect
 
 function InViewAnimate({ children, className }: InViewAnimateProps) {
   const ref = useRef(null);
